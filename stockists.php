4c7f8285d0623061e1b42e722695e02c38e4d71f
MamzelNyf <you@example.comfanny.lefert@gmail.com>
2019-08-01 16:15:15 +0800

    stockist page update, bootstrap grid adjusted

--- stockists.php
+++ stockists.php
@@ -19,18 +19,45 @@
                 </div>
             </div>
             <br class="clearfloat">
-            <section class="row">
-                <div class="col col-md-one-half map"><iframe src="https://www.google.com/maps/d/u/0/embed?mid=1MWEcXiTxag2AcU7GL0Yy96M0Vitl-zzx" ></iframe></div>
-                <div class="col col-md-one-half white">
-                    <h3>Our favorite places</h3>
-                    <p>So Freo's adventure has just started! As soon as we get new places to supply our products, we will let you know on Facebook &amp; Instagram!</p>
-                    <ul>
-                        <li><a href="https://www.mannawholefoods.com.au/" target="_blank">Manna Wholefood Cafe</a></li>
-                        <li><a href="https://www.facebook.com/IGARossmoyne/" target="_blank">Rossmoyne IGA</a></li>
-                        <li><a href="http://peachesfresh.com.au/" target="_blank">Peaches Fresh Food</a></li>
-                        <li><a href="https://www.naturesharvest.com.au/" target="_blank">Nature's Harvest</a></li>
-                        <li><a href="https://www.lavidavegan.com.au/" target="_blank">La Vida Vegan</a></li>
-                    </ul>
+            <section class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-md-4 white px-4">
+                        <h3>Meet us at the market!</h3>
+                        <p>Every week-end, come and meet Alex at the market, fortnightly at Subiaco Farmers Market and Mt Claremont market.
+                        <br>You will find all the events, tastings and markets on our Calendar!</p>
+                        <h3>Too Far? Buy online!</h3>
+                        <p>With Perth Organics, add the Veggie Paté to your box of organic and local products and get it delivered to your place! </p>
+                        <ul>
+                            <li><a href="https://www.perthorganics.com/vegpate230g" target="_blank">Veggie Paté</a><br></li>
+                            <li><a href="https://www.perthorganics.com/veggie-pate-THAI-so-freo-230g" target="_blank">Thai Veggie Paté</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-md-4"><img src="img/market.jpg" class="cover" alt="So Freo Market"></div>
+                    <div class="col-12 col-md-4 map"><iframe src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=2&amp;bgcolor=%23e6a756&amp;ctz=Australia%2FPerth&amp;src=MnZxYXBkYnJ1ZDlucmgxOXU1YmFjaWU4YWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZmJhMDdwMmtwdGswdnM3N2dza2xxMDJjbHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=OGo0dmphamgxZmRnMm9yZGNlOGl0cjlrdThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000&amp;color=%23F4511E&amp;showCalendars=0&amp;showTz=0&amp;title=Come%20to%20get%20your%20favorite%20Veggie%20Paté%20on%20the%20market!" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe></div>
+                    </div>
+            </section>
+            
+
+
+            <section class="container-fluid">
+                <div class="row mt-4">
+                    <div class="col-12 col-md-6 white order-md-2 px-4">
+                        <h3>Our favorite places</h3>
+                        <p>Check for new places where you can find your favorite Veggie Paté!
+                        <p>As soon as we get new places to supply our products, we will let you know on Facebook &amp; Instagram!</p>
+                        <div class="cell">
+                            <ul>
+                                <li><a href="https://www.mannawholefoods.com.au/" target="_blank">Manna Wholefood Cafe</a></li>
+                                <li><a href="https://www.facebook.com/IGARossmoyne/" target="_blank">Rossmoyne IGA</a></li>
+                                <li><a href="http://peachesfresh.com.au/" target="_blank">Peaches Fresh Food</a></li>
+                                <li><a href="http://www.boatshedmarket.com.au/" target="_blank">Boatshed</a></li>
+                                <li><a href="http://localfoodiehub.com.au/" target="_blank">Local Foodie Hub</a></li>
+                                <li><a href="https://www.perthorganics.com/" target="_blank">Perth Organics</a></li>
+                                <li><a href="http://www.myhealthmarket.com.au/" target="_blank">My Health Market - Go Vita</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 map order-md-1"><iframe src="https://www.google.com/maps/d/u/0/embed?mid=1MWEcXiTxag2AcU7GL0Yy96M0Vitl-zzx" ></iframe></div>
                 </div>
             </section>
             </main>
