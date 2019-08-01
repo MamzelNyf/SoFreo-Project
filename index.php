a232114a6667d7cbb087517dc640146def7b5ee3
MamzelNyf <you@example.comfanny.lefert@gmail.com>
2019-08-01 21:32:37 +0800

    index page updated

--- index.php
+++ index.php
@@ -12,41 +12,46 @@
         include 'includes/nav.php';
         ?>
         <main>
-            <div class="parallax"></div>
+            <div class="parallax section-heading">
+              <div class="homequote section-heading-upper d-none d-md-block"><span class="quotation text-black-50">"</span><div>You don't have to eat<span class="section-heading-lower">Less</span>You just have to eat<span class="section-heading-lower">Right</span></div></div>
+            </div>
             <div class="bloc">
                 <h2>So Vegan, So Good</h2>
             </div>
             <section class="page-section clearfix">
                 <div class="container">
                     <div class="intro">
-                        <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/max.jpg" alt="">
-                        <div class="intro-text left-0 text-center bg-faded p-5 rounded">
-                        <h2 class="section-heading mb-4">
-                            <span class="section-heading-upper">Fresh Coffee</span>
-                            <span class="section-heading-lower">Worth Drinking</span>
-                        </h2>
+                        <div class="intro-text left-0 text-center p-5 rounded order-md-2">
+                        <h3 class="mb-4 section-heading">
+                        <span class="section-heading-upper text-black-50">Organic, Plant-based and </span>
+                        <span class="section-heading-lower">delicious!</span>
+                        </h3>
                         <p class="mb-3">So Freo is locally based and focuses on organic and locally sourced ingredients from WA. 
-                <br>For school lunch, picnic, plancha or your next outdoor adventure.   
-                <br>Vegan, High Protein & Gluten Free, <br>it is your healthy choice.
+                        <br>For school lunch, picnic, plancha or your next outdoor adventure.   
+                        <br>Vegan, High Protein & Gluten Free, <br>it is your healthy choice.
                         </p>
                             <div class="intro-button mx-auto">
-                                <a class="btn btn-primary btn-xl" href="#">Our products</a>
+                                <a class="btn btn-primary btn-xl" href="products.php">Our products</a>
                             </div>
                         </div>
+                        <img class="intro-img img-fluid mb-3 mb-lg-0 rounded order-md-1" src="img/max.jpg" alt="Max eating Veggie Pate on the Ridge walk">
                     </div>
                 </div>
             </section>
 
-  <section class="page-section cta">
+  <section class="page-section cta mt-4">
     <div class="container">
-      <div class="row">
+      <div class="row ">
         <div class="col-xl-9 mx-auto">
-          <div class="cta-inner text-center rounded">
-            <h2 class="section-heading mb-4">
-              <span class="section-heading-upper">Our Promise</span>
-              <span class="section-heading-lower">To You</span>
-            </h2>
-            <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+          <div class="text-center cta-inner rounded">
+            <h3 class="mb-2 section-heading "><span class="section-heading-upper text-black-50">Give it a</span> <span class="section-heading-lower">try!</span></h3>
+            <p class="mb-4">Vegan or not, Gluten intolerant or not, young or old, the Veggie Paté is for you.
+            <br>When we decide to elaborate its recipe, we wanted to add the best in it, organic veggies, a yummy taste and a lot of love!
+            <br>We hope you will really enjoy to share it!</p>
+            <div class="align-content-center">
+              <a href="https://www.facebook.com/sofreo" class="socialicon" target="_blank"><img src="img/facebook.svg" width="20"alt="Facebook logo"></a>
+              <a href="https://www.instagram.com/sofreocuisine" class="socialicon" target="_blank"><img src="img/insta.svg" width="20" alt="Instagram logo"></a>
+            </div>
           </div>
         </div>
       </div>
